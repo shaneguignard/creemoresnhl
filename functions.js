31e334a900d183e198d7aa8ab655229447dfb2ee
Shane <shaneguignard@Shanes-MacBook-Pro.local>
2018-10-12 12:34:34 -0400

    cleaning up code

--- functions.js
+++ functions.js
@@ -1,19 +1,16 @@
 //script for the display of dynamic information
-
-
 //Identify what year the information belongs to
-
 //Make these integers that are manually populated by using todays current date
 
 var t = new Date();
 var newSeason = 6 //Declare when season ends and switches over to new year (0-11 months)
 
 var currentSeasonYear, lastSeasonYear
-if (t.getMonth()>newSeason){
+if (t.getMonth() > newSeason){
 	currentSeasonYear = t.getFullYear() + "/" + (t.getFullYear() + 1);
 	lastSeasonYear = (t.getFullYear() - 1) + "/" + (t.getFullYear());
 }
-else if(t.getMonth() <newSeason) {
+else if(t.getMonth() < newSeason) {
 	currentSeasonYear = (t.getFullYear() - 1) + "/" + t.getFullYear();
 	lastSeasonYear = (t.getFullYear() - 2) + "/" + (t.getFullYear() - 1);
 }
@@ -22,193 +19,108 @@ console.log("Current Season: " + currentSeasonYear);
 console.log("Last Season: " + lastSeasonYear);
 
 //update webpage year
-
 //assign cy (current year) and ly (last year) array
-
 var cy = [];
-
 var ly = [];
 
-
 //assign cy array to "currentSeasonYear" class
-
 cy = document.getElementsByClassName("currentSeasonYear");
 
-
-
 //for every class with "currentSeasonYear" apply current year date to innerHTML
-
 for (var i = 0; i < cy.length; i++) {
-
     cy[i].innerHTML = currentSeasonYear;
-
 }
 
-
-
 //assign ly array to "lastSeasonYear" class
-
 ly = document.getElementsByClassName("previousYear");
 
 //for every tag with class "lastSeasonYear" apply last year date to innerHTML
-
 for (var i = 0; i < ly.length; i++) {
-
     ly[i].innerHTML = lastSeasonYear;
-
 }
 
-
-
 // Season Winners
-
 var previousYearWinners = "Belarus";
-
 document.getElementById("seasonWinners").innerHTML = previousYearWinners;
 
 //Supporting Picture
-
 var champsPicture = "images/2017_winners.jpeg";
-
 document.getElementById("featureImg").src = champsPicture;
-
 
 
 //Schedule.js 
 
-
-
-
-
 //Define months of the year. 
-
 var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-
-
 var deskSchedule = [];
-
 var mobileSchedule = [];
-
 var game = [];
-
 var gd = [];
-
 var gameDay = [];
-
 var gameMon = [];
-
 var gameYear = [];
-
 var gameDate = [];
-
-
-
 var foundgame = 0;
 
-
 function schedule_desk() {
-
     $.ajax({
-
         type: "GET",
-
         // Call Json file
-		// find a way to load text file
+	// find a way to load text file
         url: t.getFullYear() + "/schedule.csv",
 
         //Declare Json data type
-
         dataType: "json",
-
         cache: false,
-
         contentType: "application/json",
-
         //Function launched if data is loaded successfully from JSON file specified above
 
         success: function (data) {
-			console.log("schedule: " + data)
-            if (data) {
-
-                deskSchedule.push("<tr><th></th><th colspan='3'>7:00PM</th><th colspan='3'>8:00PM</th><th colspan='3'>9:00PM</th></tr> <tr><th>Date</th><th>Home</th><th></th><th>Away</th><th >Home</th><th></th><th>Away</th><th>Home</th><th></th><th>Away</th></tr>");
-
-
-
-                for (i = 2; i < data.length; i++) {
-
-
-
-                    gd[i] = data[i][0] - 25568;
-
-                    gd[i] = gd[i] * 24 * 60 * 60 * 1000;
-
-                    gameDay[i] = new Date(gd[i]).getDate();
-
-                    gameMon[i] = new Date(gd[i]).getMonth();
-
-                    gameYear[i] = new Date(gd[i]).getFullYear();
-
-                    gameDate[i] = month[gameMon[i]] + ' ' + gameDay[i] + ', ' + gameYear[i];
-
-
+		console.log("schedule: " + data)
+            	if (data) {
+        	       	deskSchedule.push("<tr><th></th><th colspan='3'>7:00PM</th><th colspan='3'>8:00PM</th><th colspan='3'>9:00PM</th></tr> <tr><th>Date</th><th>Home</th><th></th><th>Away</th><th >Home</th><th></th><th>Away</th><th>Home</th><th></th><th>Away</th></tr>");
+ 			for (i = 2; i < data.length; i++) {
+	                    gd[i] = data[i][0] - 25568;
+	                    gd[i] = gd[i] * 24 * 60 * 60 * 1000;
+	                    gameDay[i] = new Date(gd[i]).getDate();
+	                    gameMon[i] = new Date(gd[i]).getMonth();
+	                    gameYear[i] = new Date(gd[i]).getFullYear();
+	                    gameDate[i] = month[gameMon[i]] + ' ' + gameDay[i] + ', ' + gameYear[i];
 
                     if (data[i][1] == '') {
-
                         break;
-
                     }
 
                     deskSchedule.push("<tr><th>" + gameDate[i] + "</th><td class='" + data[i][1] + "'>" + data[i][1] + "</td><td>vs</td><td class='" + data[i][3] + "'>" + data[i][3] + "</td><td class='" + data[i][5] + "'>" + data[i][5] + "</td><td>vs</td><td class='" + data[i][7] + "'>" + data[i][7] + "</td><td class='" + data[i][9] + "'>" + data[i][9] + "</td><td>vs</td><td class='" + data[i][11] + "'>" + data[i][11] + "</td></tr>");
-
-
-
                     mobileSchedule.push("<tr><th colspan='3'>" + gameDate[i] + "</th></tr><tr><th colspan='3'>7PM</th></tr><tr><td>" + data[i][1] + "</td><td>vs</td><td>" + data[i][3] + "</td></tr><tr><th colspan='3'>8PM</th></tr><tr><td>" + data[i][5] + "</td><td>vs</td><td>" + data[i][7] + "</td></tr><tr><th colspan='3'>9PM</th></tr><tr><td>" + data[i][9] + "</td><td>vs</td><td>" + data[i][11] + "</td></tr>");
-
                 }
-
             }
 
             var mobileResults = mobileSchedule.join('');
-
             var results = deskSchedule.join('');
-
             document.getElementById('schedule_table').innerHTML = results;
-
             document.getElementById('mobileSchedule').innerHTML = mobileResults;
-
             return
-
         },
 
-
-
         //Function launched if data  loaded from JSON file contains errors
-
         error: function (jqXHR, textStatus, errorThrown) {
 
             //Outputs Error Status information in console
-
             console.log('ERROR', textStatus, errorThrown);
-
         }
-
     });
 
 }
 
 
-
 var nextGameArray = [];
-
 function loadNextGame() {
 
     //perform Json parse using Jquery
-
     $.ajax({
-
         type: "GET",
-
         // Call Json file
 
         url: "data/gamestats.json",
